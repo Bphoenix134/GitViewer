a09fb853a775057b7c5a323c748c96b0b0ec640f
Kris <blackphoenix134@mail.ru>
2025-12-05 21:03:47 +0300

    feat(remote): create Api for interaction with GitHubApi Added methods for using endpoints: getListOfRepo(), getFullInfoAboutRepo, getContentRepo

---
+++ shared/src/commonMain/kotlin/data/remote/GitHubApi.kt
@@ -0,0 +1,34 @@
+package data.remote
+
+import data.remote.dto.RepoContentDto
+import data.remote.dto.RepoDetailedDto
+import data.remote.dto.RepoDto
+import data.remote.dto.SearchResponseDto
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import kotlinx.serialization.json.Json
+
+class GitHubApi(private val client: HttpClient) {
+    private val base = "https://api.github.com"
+
+    suspend fun getListOfRepo(query: String): List<RepoDto> {
+        val response: SearchResponseDto =
+            client.get("$base/search/repositories") {
+                parameter("q", query)
+            }.body()
+
+        return response.items
+    }
+
+    suspend fun getFullInfoAboutRepo(owner: String, repo: String): RepoDetailedDto {
+        return client.get("$base/repos/$owner/$repo").body()
+    }
+
+    suspend fun getContentOfRepo(owner: String, repo: String): List<RepoContentDto> {
+        return client.get("$base/repos/$owner/$repo/contents").body()
+    }
+}
+
+fun defaultJson() = Json { ignoreUnknownKeys = true }
