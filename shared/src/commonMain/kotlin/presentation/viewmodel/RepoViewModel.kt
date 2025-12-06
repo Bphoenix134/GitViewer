4137493c5ff646f6b564947a72fadf285d57f95c
Kris <blackphoenix134@mail.ru>
2025-12-07 01:51:30 +0300

    feat(vm): create vm for using repositories Added at vm function:  searchRepositories, loadRepositoryDetails, loadRepositoryContents

---
+++ shared/src/commonMain/kotlin/presentation/viewmodel/RepoViewModel.kt
@@ -0,0 +1,65 @@
+package presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import domain.usecase.GetRepositoryContentsUseCase
+import domain.usecase.GetRepositoryDetailsUseCase
+import domain.usecase.SearchRepositoriesUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import presentation.viewmodel.state.ContentState
+import presentation.viewmodel.state.DetailsState
+import presentation.viewmodel.state.SearchState
+
+class RepoViewModel(
+    private val searchRepositoriesUseCase: SearchRepositoriesUseCase,
+    private val getRepositoryDetailsUseCase: GetRepositoryDetailsUseCase,
+    private val getRepositoryContentsUseCase: GetRepositoryContentsUseCase
+) : ViewModel() {
+
+    private val _searchState = MutableStateFlow<SearchState>(SearchState.Idle)
+    val searchState: StateFlow<SearchState> = _searchState
+
+    private val _detailsState = MutableStateFlow<DetailsState>(DetailsState.Idle)
+    val detailsState: StateFlow<DetailsState> = _detailsState
+
+    private val _contentState = MutableStateFlow<ContentState>(ContentState.Idle)
+    val contentState: StateFlow<ContentState> = _contentState
+
+    fun searchRepositories(query: String) {
+        viewModelScope.launch {
+            _searchState.value = SearchState.Loading
+            try {
+                val list = searchRepositoriesUseCase(query)
+                _searchState.value = SearchState.Success(list)
+            } catch (e: Exception) {
+                _searchState.value = SearchState.Error(e.message ?: "Unknown error")
+            }
+        }
+    }
+
+    fun loadRepositoryDetails(owner: String, repo: String) {
+        viewModelScope.launch {
+            _detailsState.value = DetailsState.Loading
+            try {
+                val details = getRepositoryDetailsUseCase(owner, repo)
+                _detailsState.value = DetailsState.Success(details)
+            } catch (e: Exception) {
+                _detailsState.value = DetailsState.Error(e.message ?: "Unknown error")
+            }
+        }
+    }
+
+    fun loadRepositoryContents(owner: String, repo: String) {
+        viewModelScope.launch {
+            _contentState.value = ContentState.Loading
+            try {
+                val list = getRepositoryContentsUseCase(owner, repo)
+                _contentState.value = ContentState.Success(list)
+            } catch (e: Exception) {
+                _contentState.value = ContentState.Error(e.message ?: "Unknown error")
+            }
+        }
+    }
+}
